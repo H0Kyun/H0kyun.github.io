5beed45368fbe7df0bdbadacd8f2b893bc87fc8b
Hokyun <h0kyungim@gmail.com>
2022-02-16 11:24:59 +0900

    reset blog

--- js/side.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-   document.getElementById("sidesection").style.left = 0;
-   document.getElementById("sidelabel").style.opacity = 0;
-}
-
-function closeNav() {
-    document.getElementById("sidesection").style.left = '-200px';
-    document.getElementById("sidelabel").style.opacity = 1;
-}
